3c5533e087b2cd752b742c01029b1941820df6d6
OwnDing <jianchending@outlook.com>
2026-05-02 13:27:21 +0800

    登录地址变更

--- android/src/main/java/com/larktun/ipn/ui/config/MeshyraBackendConfig.kt
+++ android/src/main/java/com/larktun/ipn/ui/config/MeshyraBackendConfig.kt
@@ -8,6 +8,6 @@ package com.larktun.ipn.ui.config
  * Keep this file obvious/easy to find so backend URLs can be updated quickly.
  */
 object MeshyraBackendConfig {
-  const val AUTH_BASE_URL = "http://182.61.39.195:3001"
+  const val AUTH_BASE_URL = "https://larktun.com"
 }
 
